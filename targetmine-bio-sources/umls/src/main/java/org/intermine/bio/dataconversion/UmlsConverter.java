2ca5ccbfcaa2f5d73ece95fd9d03e5fcf25c32e4
Kubota.Hiroshi <Kubota.Hiroshi.f@cbo.mss.co.jp>
2019-07-03 11:27:16 +0900

    umls insert data.

--- targetmine-bio-sources/umls/src/main/java/org/intermine/bio/dataconversion/UmlsConverter.java
+++ targetmine-bio-sources/umls/src/main/java/org/intermine/bio/dataconversion/UmlsConverter.java
@@ -10,7 +10,9 @@ package org.intermine.bio.dataconversion;
  *
  */
 
+import java.io.BufferedReader;
 import java.io.Reader;
+import java.util.HashSet;
 
 import org.intermine.dataconversion.ItemWriter;
 import org.intermine.metadata.Model;
@@ -24,8 +26,8 @@ import org.intermine.xml.full.Item;
 public class UmlsConverter extends BioFileConverter
 {
     //
-    private static final String DATASET_TITLE = "Add DataSet.title here";
-    private static final String DATA_SOURCE_NAME = "Add DataSource.name here";
+    private static final String DATASET_TITLE = "2018AB";
+    private static final String DATA_SOURCE_NAME = "UMLS";
 
     /**
      * Constructor
@@ -42,6 +44,21 @@ public class UmlsConverter extends BioFileConverter
      * {@inheritDoc}
      */
     public void process(Reader reader) throws Exception {
-
+        try(BufferedReader reader1 = new BufferedReader(reader)){
+            String line = null;
+            HashSet<String> idSet = new HashSet<>();
+            while((line = reader1.readLine())!=null){
+                String[] split = line.split("\\|");
+                Item umlsDisease = createItem("UMLSDisease");
+                String identifer = split[0];
+                if(idSet.contains(identifer)) {
+                    continue;
+                }
+                umlsDisease.setAttribute("identifier",identifer);
+                String name = split[14];
+                umlsDisease.setAttribute("name",name);
+                store(umlsDisease);
+            }
+        }
     }
 }
