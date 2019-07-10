6e32eb2a8467fa235f765c430bbe39aaa4594538
Uehara.Keizo <Uehara.Keizo@mss.co.jp>
2019-07-10 11:47:52 +0900

    WIP

--- targetmine-bio-sources/utils/src/main/java/org/intermine/bio/dataconversion/UMLSResolver.java
+++ targetmine-bio-sources/utils/src/main/java/org/intermine/bio/dataconversion/UMLSResolver.java
@@ -1,26 +1,18 @@
 package org.intermine.bio.dataconversion;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 
 public class UMLSResolver {
-	private UMLSParser parser;
 	private HashMap<String, String> nameIdMap = new HashMap<String, String>();
 
-	public UMLSResolver(File consoFile,File styFile) throws FileNotFoundException {
-		this.parser = new UMLSParser(consoFile, styFile);
-		parser.setSemanticTypes(UMLSParser.DATA_TYPES);
-		try {
-			loadData(consoFile,styFile);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+	public UMLSResolver(File consoFile,File styFile) throws IOException {
+		loadData(consoFile,styFile);
 	}
 
 	public void loadData(File consoFile,File styFile) throws IOException {
-		try(UMLSParser parser = new UMLSParser(consoFile, styFile)){
+		try(UMLSParser parser = new UMLSParser(consoFile, styFile,UMLSParser.DATA_TYPES)){
 			UMLS umls = null;
 			while((umls = parser.getNext())!=null) {
 				nameIdMap.put(umls.getName().toLowerCase(), umls.getIdentifier());
