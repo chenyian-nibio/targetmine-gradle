3e7c113ceebc5759cfc865dc31fdf8105de8ae33
Uehara.Keizo <Uehara.Keizo@mss.co.jp>
2019-12-13 14:11:04 +0900

    Implements COTDTrialConverter

--- targetmine-bio-sources/utils/src/main/java/org/intermine/bio/dataconversion/CSVParser.java
+++ targetmine-bio-sources/utils/src/main/java/org/intermine/bio/dataconversion/CSVParser.java
@@ -5,13 +5,21 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 public class CSVParser implements Iterable<Map<String,String>>,AutoCloseable{
+	private boolean multiColumnHeader = false; 
 	private BufferedReader reader;
 	
+	public CSVParser(Reader reader,boolean multiColumnHeader) {
+		super();
+		this.reader = new BufferedReader(reader);
+		this.multiColumnHeader = multiColumnHeader;
+	}
 	public CSVParser(Reader reader) {
 		super();
 		this.reader = new BufferedReader(reader);
@@ -23,16 +31,51 @@ public class CSVParser implements Iterable<Map<String,String>>,AutoCloseable{
 			reader = null;
 		}
 	}
+	private String[] headerTypes;
+	private String[] headers;
+	public Map<String,List<String>> getHeaderGroups(){
+		HashMap<String, List<String>> map = new HashMap<>();
+		for (int i = 0; i < headers.length; i++) {
+			String type = headerTypes[Math.min(i, headerTypes.length-1)];
+			List<String> list = map.get(type);
+			if(list==null) {
+				list = new ArrayList<String>();
+				map.put(type, list);
+			}
+			list.add(headers[i]);
+		}
+		return map;
+	}
 	class RowIterator implements Iterator<Map<String, String>>{
-		private String[] headers;
 		private Map<String, String> row;
+		public RowIterator() {
+			try {
+				readHeaders();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		private void readHeaders() throws IOException {
+			if(multiColumnHeader) {
+				String readLine = reader.readLine();
+				String[] cols = splitLine(readLine);
+				String prevHeader = null;
+				for (int i = 0; i < cols.length; i++) {
+					if(Utils.empty(cols[i])) {
+						cols[i] = prevHeader;
+					}
+					prevHeader = cols[i];
+				}
+				CSVParser.this.headerTypes = cols;
+			}
+			String readLine = reader.readLine();
+			if(readLine==null) {
+				throw new RuntimeException("No line");
+			}
+			headers = splitLine(readLine);
+		}
 		public boolean readLine() throws IOException {
 			if(headers==null) {
-				String readLine = reader.readLine();
-				if(readLine==null) {
-					throw new RuntimeException("No line");
-				}
-				headers = splitLine(readLine);
 			}
 			String readLine = reader.readLine();
 			if(readLine==null) {
