d6e8feaf1811ebb4f75946d3a8d940e28ca8923f
Yi-An Chen <chenyian@nibiohn.go.jp>
2024-05-23 15:18:59 +0900

    add a new annotation, target-based classification from kegg brite

--- targetmine-bio-sources/kegg-jtc/src/main/java/org/intermine/bio/dataconversion/KeggJtcConverter.java
+++ targetmine-bio-sources/kegg-jtc/src/main/java/org/intermine/bio/dataconversion/KeggJtcConverter.java
@@ -5,27 +5,19 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.intermine.dataconversion.FileConverter;
 import org.intermine.dataconversion.ItemWriter;
 import org.intermine.metadata.Model;
-import org.intermine.model.InterMineObject;
-import org.intermine.objectstore.ObjectStore;
 import org.intermine.objectstore.ObjectStoreException;
-import org.intermine.objectstore.ObjectStoreFactory;
-import org.intermine.objectstore.query.Query;
-import org.intermine.objectstore.query.QueryClass;
-import org.intermine.objectstore.query.Results;
-import org.intermine.objectstore.query.ResultsRow;
 import org.intermine.xml.full.Item;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 
@@ -34,32 +26,28 @@ import org.intermine.xml.full.Item;
 public class KeggJtcConverter extends FileConverter {
 	
 	protected static final Logger LOG = LogManager.getLogger(KeggJtcConverter.class);
-    //
+	//
 	Map<String, Integer> codeMap = new HashMap<String, Integer>();
 	
-    /**
-     * Constructor
-     * @param writer the ItemWriter used to handle the resultant items
-     * @param model the Model
-     */
-    public KeggJtcConverter(ItemWriter writer, Model model) {
-        super(writer, model);
-        codeMap.put("A", 1);
-        codeMap.put("B", 2);
-        codeMap.put("C", 3);
-        codeMap.put("D", 4);
-    }
+	/**
+	 * Constructor
+	 * @param writer the ItemWriter used to handle the resultant items
+	 * @param model the Model
+	 */
+	public KeggJtcConverter(ItemWriter writer, Model model) {
+		super(writer, model);
+		codeMap.put("A", 1);
+		codeMap.put("B", 2);
+		codeMap.put("C", 3);
+		codeMap.put("D", 4);
+	}
 
-    /**
-     * 
-     *
-     * {@inheritDoc}
-     */
-    public void process(Reader reader) throws Exception {
-    	if (drugCompoundIdMap == null || drugCompoundIdMap.isEmpty()) {
-    		getDrugBankIdMap();
-    	}
-    	
+	/**
+	 * 
+	 *
+	 * {@inheritDoc}
+	 */
+	public void process(Reader reader) throws Exception {
 		String currentFileName = getCurrentFile().getName();
 		if (currentFileName.equals("br08301.keg")) {
 			BufferedReader in = new BufferedReader(reader);
@@ -79,17 +67,8 @@ public class KeggJtcConverter extends FileConverter {
 				} else if (line.startsWith("E")) {
 					String keggDrugId = line.substring(9, 15);
 					if (currentClassId.length() == 4) {
-						Set<String> identifiers = drugCompoundIdMap.get(keggDrugId);
-						if (identifiers != null) {
-							for (String drugCompoundId : identifiers) {
-								Item drugCompound = getDrugCompound(drugCompoundId);
-								drugCompound.addToCollection("jsccCodes", jsccMap.get(currentClassId));
-							}
-						} else {
-							LOG.info("Unfound entry: " + keggDrugId);
-							Item drugCompound = getKeggDrugCompound(keggDrugId);
-							drugCompound.addToCollection("jsccCodes", jsccMap.get(currentClassId));
-						}
+						Item drugCompound = getKeggDrugCompound(keggDrugId);
+						drugCompound.addToCollection("jsccCodes", jsccMap.get(currentClassId));
 
 					} else {
 						throw new RuntimeException("illeagle format at line: " + line);
@@ -101,35 +80,22 @@ public class KeggJtcConverter extends FileConverter {
 			// #DEFINITION  USP Drug Classification
 			BufferedReader in = new BufferedReader(reader);
 			String line;
-//			List<String> parents = new ArrayList<String>();
 			Map<String, String> parentMap = new HashMap<String, String>();
 			while ((line = in.readLine()) != null) {
 				if (line.startsWith("A")) {
-					// reset all parents
-//					parents.clear();
 					parentMap.clear();
 					String classA = createUspClassification("A", line.substring(1), null);
-//					parents.add(classA);
 					parentMap.put("A", classA);
-				} else if (line.substring(0, 1).matches("[BCD]")) {
+				} else if (line.substring(0, 1).matches("[BCDE]")) {
 					if (line.matches(".+\\sD\\d{5}\\s{2}.+")) {
 						String[] split = line.split("\\s+", 3);
 						String keggDrugId = split[1].trim();
-						Set<String> identifiers = drugCompoundIdMap.get(keggDrugId);
 						if (!parentMap.isEmpty()) {
-				    		List<String> keys = new ArrayList<String>(parentMap.keySet());
-				    		Collections.sort(keys);
-				    		String parentId = parentMap.get(keys.get(keys.size() - 1));
-							if (identifiers != null) {
-								for (String drugCompoundId : identifiers) {
-									Item drugCompound = getDrugCompound(drugCompoundId);
-									drugCompound.addToCollection("uspClassifications", parentId);
-								}
-							} else {
-								LOG.info("Unfound entry: " + keggDrugId);
-								Item drugCompound = getKeggDrugCompound(keggDrugId);
-								drugCompound.addToCollection("uspClassifications", parentId);
-							}
+							List<String> keys = new ArrayList<String>(parentMap.keySet());
+							Collections.sort(keys);
+							String parentId = parentMap.get(keys.get(keys.size() - 1));
+							Item drugCompound = getKeggDrugCompound(keggDrugId);
+							drugCompound.addToCollection("uspClassifications", parentId);
 						}
 					} else {
 						String[] split = line.split("\\s+", 2);
@@ -137,32 +103,80 @@ public class KeggJtcConverter extends FileConverter {
 							String name = split[1];
 							name = name.replaceAll("\\[DG:DG.+\\]", "");
 							String code = line.substring(0, 1);
-//							if (parentMap.containsKey(code)) {
-//								parentMap.remove(code);
-//							}
 							String classId = createUspClassification(code, name.trim(), parentMap);
-//							parents.add(classId);
 							parentMap.put(code, classId);
 						}
 					}
 				}	
 			}
 			in.close();
+		} else if (currentFileName.equals("br08310.keg")) {
+			BufferedReader in = new BufferedReader(reader);
+			String line;
+			Map<String, String> parentMap = new HashMap<String, String>();;
+			List<String> genes = new ArrayList<String>();
+			String target = "";
+			while ((line = in.readLine()) != null) {
+				if (line.startsWith("C") || line.startsWith("B") || line.startsWith("A")) {
+					String replaceAll = line.replaceAll("<\\/?b>", " ");
+					String[] split = replaceAll.split("\\s+", 2);
+					String lv = split[0];
+					String name = split[1].trim();
+					if (lv.equals("C")) {
+						parentMap.put(lv, getTargetBasedClassification(codeMap.get(lv), name, parentMap.get("B")));
+					} else if (lv.equals("B")) {
+						parentMap.put(lv, getTargetBasedClassification(codeMap.get(lv), name, parentMap.get("A")));
+					} else if (lv.equals("A")) {
+						parentMap.put(lv, getTargetBasedClassification(codeMap.get(lv), name, null));
+					} else {
+						throw new RuntimeException("Unexpected level: '" + lv + "' at the line: '" + line + "'.");
+					} 
+				} else if (line.startsWith("D")) {
+					Pattern p = Pattern.compile("\\s+(.+)\\s+\\[HSA:(.+)\\]\\s");
+					Matcher m = p.matcher(line);
+					if (m.find()) {
+						target = m.group(1);
+						String[] geneIds = m.group(2).split(" ");
+						genes = new ArrayList<String>();
+						for(String id : geneIds) {
+							genes.add(getGene(id));
+						}
+					} else {
+						System.err.println("No match. " + line);
+					}
+				} else if (line.startsWith("E")) {
+					String keggDrugId = line.substring(9, 15);
+					Item drugCompound = getKeggDrugCompound(keggDrugId);
+					
+					Item annotation = createItem("TbcAnnotation");
+					annotation.setAttribute("target", target);
+					annotation.setReference("classification", parentMap.get("C"));
+					for (String g : genes) {
+						annotation.addToCollection("genes", g);
+					}
+					annotation.setReference("drug", drugCompound);
+					store(annotation);
+				} else {
+					// throw new RuntimeException("Unexpected initial at the line: '" + line + "'.");
+					System.out.println("Skip the line: '" + line + "'");
+				}
+			}
+			in.close();
+
 		} else {
 			System.out.println("Unexpected file: " + currentFileName + ", skip it.");
 			return;
 		}
-    	
-    }
 
-    @Override
-    public void close() throws Exception {
-    	store(drugCompoundMap.values());
-    }
-    
-    private Map<String, Item> drugCompoundMap = new HashMap<String, Item>();
+	}
 
-    // Though this should be never used
+	@Override
+	public void close() throws Exception {
+		store(drugCompoundMap.values());
+	}
+
+	private Map<String, Item> drugCompoundMap = new HashMap<String, Item>();
+
     private Item getKeggDrugCompound(String keggDrugId) throws ObjectStoreException {
     	Item ret = drugCompoundMap.get(keggDrugId);
     	if (ret == null) {
@@ -174,16 +188,6 @@ public class KeggJtcConverter extends FileConverter {
     	return ret;
     }
     
-    private Item getDrugCompound(String identifier) throws ObjectStoreException {
-    	Item ret = drugCompoundMap.get(identifier);
-    	if (ret == null) {
-    		ret = createItem("DrugCompound");
-    		ret.setAttribute("identifier", identifier);
-    		drugCompoundMap.put(identifier, ret);
-    	}
-    	return ret;
-    }
-
     private Map<String, String> jsccMap = new HashMap<String, String>();
     
     private String createJscClassification(String jsccCode, String name, String parentId) throws ObjectStoreException {
@@ -225,45 +229,34 @@ public class KeggJtcConverter extends FileConverter {
     	return ret;
     }
 
-	private String osAlias = null;
-
-	public void setOsAlias(String osAlias) {
-		this.osAlias = osAlias;
-	}
-
-	private Map<String, Set<String>> drugCompoundIdMap;
-
-	@SuppressWarnings("unchecked")
-	private void getDrugBankIdMap() throws Exception {
-		drugCompoundIdMap = new HashMap<String, Set<String>>();
-
-		ObjectStore os = ObjectStoreFactory.getObjectStore(osAlias);
-
-		Query q = new Query();
-		QueryClass qcDrugCompound = new QueryClass(os.getModel()
-				.getClassDescriptorByName("DrugCompound").getType());
-
-
-		q.addFrom(qcDrugCompound);
-
-		q.addToSelect(qcDrugCompound);
-
-		Results results = os.execute(q);
-		Iterator<Object> iterator = results.iterator();
-		while (iterator.hasNext()) {
-			ResultsRow<InterMineObject> rr = (ResultsRow<InterMineObject>) iterator.next();
-			InterMineObject p = rr.get(0);
-
-			String identifier = (String) p.getFieldValue("identifier");
-			String keggDrugId = (String) p.getFieldValue("keggDrugId");
-			
-			if (identifier != null && keggDrugId != null) {
-				if (drugCompoundIdMap.get(keggDrugId) == null) {
-					drugCompoundIdMap.put(keggDrugId, new HashSet<String>());
-				}
-				drugCompoundIdMap.get(keggDrugId).add(identifier);
+	private Map<String, String> tbcMap = new HashMap<String, String>();
+	private String getTargetBasedClassification(Integer level, String name, String parentRefId) throws ObjectStoreException {
+		String ret = tbcMap.get(name);
+		if (ret == null) {
+			Item item = createItem("TargetBasedClassification");
+			item.setAttribute("lv", String.valueOf(level));
+			item.setAttribute("name", name);
+			if (parentRefId != null) {
+				item.setReference("parent", parentRefId);
 			}
-
+			store(item);
+			ret = item.getIdentifier();
+			tbcMap.put(name, ret);
 		}
+		return ret;
 	}
+
+	private Map<String, String> geneMap = new HashMap<String, String>();
+	private String getGene(String geneId) throws ObjectStoreException {
+		String ret = geneMap.get(geneId);
+		if (ret == null) {
+			Item item = createItem("Gene");
+			item.setAttribute("primaryIdentifier", geneId);
+			store(item);
+			ret = item.getIdentifier();
+			geneMap.put(geneId, ret);
+		}
+		return ret;
+	}
+
 }
